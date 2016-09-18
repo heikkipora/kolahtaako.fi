fd58d4848a7d016b02c9b42c8d95090eedb2da8c
Heikki Pora <heikki.pora@reaktor.fi>
2016-09-18 13:22:31 +0300

    Styled header

--- work/optimize.sh
+++ work/optimize.sh
@@ -17,3 +17,6 @@ optimize the-big-short-steve-carell-ryan-gosling.jpg ../img/the_big_short.jpg
 optimize TMKauniin.jpg ../img/teit_meista_kauniin.jpg
 optimize Tulen_morsian_pressi3.jpg ../img/tulen_morsian.jpg
 optimize TyttoNimeltaVarpu1_photo_Tuo_moHutri_MakingMovies2016.jpg ../img/tytto_nimelta_varpu.jpg
+
+# original 3511x2096
+convert Teemakuva.jpg -strip -interlace Plane -sampling-factor 4:2:0 -quality 85% -resize 752x448 -gravity center -extent 752x448 ../img/header.jpg
